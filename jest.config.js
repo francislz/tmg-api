db2ca359355e6b5d59ff3f301e491547882cb5fe
francislz <francisconeluiz@gmail.com>
2024-10-31 14:34:56 -0300

    fix: typescript configurations for jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  modulePaths: ['<rootDir>'], // <-- This will be set to 'baseUrl' value
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
